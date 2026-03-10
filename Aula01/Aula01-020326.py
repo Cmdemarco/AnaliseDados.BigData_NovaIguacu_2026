de60af1102469ee0a60d0bea20a17c4ec359da1f
Cmdemarco <caio.pais@outlook.com>
2026-03-09 21:17:53 -0300

    Primeiro Material

---
+++ Aula01/Aula01-020326.py
@@ -0,0 +1,70 @@
+# AULA 01 - 02/03/2026 & 04/03/2026
+# ASSUNTOS: TIPOS DE DADOS 
+
+print('Hello World') #PRINT = IMPRIMIR RESPOSTA NO TERMINAL
+
+pokebola_01='bulbassaur'
+pokebola_02='psyduck'
+pokebola_03='clefairy'
+idade=21
+nome='caio'
+preco=19.99
+
+'''#DOCSTRING '''
+# type = TIPO DE CLASSE DO DADO
+
+# data_01='04/03/2026'
+# vazio=None
+# print(vazio)
+# print(type(vazio))
+
+
+# DADOS BOOLEANOS
+decisao_01=True
+decisao_02=False
+
+print(decisao_01)
+print(type(decisao_01))
+
+
+# DESAFIO - CRIAÇÃO DE ALGORITIMO 
+
+# 1) BOLETIM: criem um algoritmo 
+# que associe duas notas de 
+# estudantes a duas variáveis 
+# criadas e calculem a média desse 
+# estudante e guardem em uma 
+# terceira variável.
+
+# 2) CALCULADORA: construa um 
+# algoritmo no qual o usuário 
+# digitará 2 números e o programa 
+# exibirá o resultado das quatro 
+# operações básicas da 
+# matemática (soma, subtração, 
+# divisão, multiplicação) e módulo
+
+
+
+Nota_Ana=9.8
+Nota_Clara=7.5
+
+Medias_das_Notas=(Nota_Ana+Nota_Clara)/2
+
+print('Questão 1.')
+print('Na escola Ana tirou 9,8 na prova e Clara tirou 7,5, qual seria a média de suas notas?')
+print('A média das notas seria:')
+print(Medias_das_Notas)
+
+print ('Questão 2.')
+num1=3
+num2=4
+
+soma=num1+num2
+subtracao=num1-num2
+divisao=num1/num2
+multiplicao=num1*num2
+modulo=num1%num2
+
+print(soma,subtracao,divisao,multiplicao,modulo, '\n')
+
---
+++ Aula02/Aula02-040326.py
@@ -0,0 +1,28 @@
+## ESTRUTURA DE DECISÃO;
+##########                                CONDIÇÕES
+
+#EX1: DUAS POSSIBILIDADES
+chover=True
+rafa_presenca=None
+
+if chover ==True:
+    rafa_presenca=False
+else:
+    rafa_presenca=True
+
+
+#= Armazenar algo dentro da variavel
+#== Comparação
+#!= Diferente
+
+#EX2: MAIS DE DUAS POSSIBILIDADES
+# placar_time1=None
+# placar_time2=None
+# resultado=None
+
+# if  placar_time1 > placar_time2:
+#     resultado= 'Time 1 ganhou a partida!'
+# elif placar_time1 < placar_time2:
+#     resultado= 'Time 2 ganhou a partida!'
+# else:
+#     resultado='Empate'
