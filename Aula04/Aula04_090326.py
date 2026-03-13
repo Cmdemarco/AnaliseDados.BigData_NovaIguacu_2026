23cfffb198e07efa8ab196c5dd52ae79ec35751e
Cmdemarco <caio.pais@outlook.com>
2026-03-13 18:28:56 -0300

    Material dia 09.03.2026

---
+++ Aula04/Aula04_090326.py
@@ -0,0 +1,22 @@
+# GITHUB
+
+# Meu Github: 
+
+# https://github.com/Cmdemarco/AnaliseDados.BigData_NovaIguacu_2026.git 
+
+
+# COMMIT (FIXAR ALTERAÇÃO NO REPOSITÓRIO) 
+# NO TERMINAL (REPOSITÓRIO DO GITHUB) 
+# git config --global user.name "Seu Nome": Define o nome que aparecerá no histórico.  
+# git config --global user.email "seuemail@exemplo.com": Define o e-mail atrelado aos seus commits 
+
+ 
+
+# COMANDOS NO TERMINAL 
+# $ git config --global user.name "Cmdemarco" 
+# $ git config --global user.email caio.pais@outlook.com 
+# $ git init 
+# $git clone 
+# $ git checkout -b main  
+
+ 
