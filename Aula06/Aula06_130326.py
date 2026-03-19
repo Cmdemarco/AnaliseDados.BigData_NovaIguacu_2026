0ae715e060df1117623f053cc223b98159ec56f6
Cmdemarco <caio.pais@outlook.com>
2026-03-18 21:57:30 -0300

    Material 13/03 &  18/06 - Aula06

--- Aula06/Aula06_130326.py
+++ Aula06/Aula06_130326.py
@@ -32,22 +32,93 @@
 # print("Estarei lá")
 
 
+############################################
+nomes = ['Matheus','Alice','Caio','Larissa','Miguel','Rafael']
+print(nomes)
+# nome1 = nomes[0]
 
-# LISTA
-#indices:   0          1        2       3          4
-nomes = ['Matheus', 'Alice', 'Caio', 'Larissa', 'Miguel']
-nome1 = (nomes [2])
+# print(nomes[-1])
+# print(nomes[-2])
+# print(nomes[-3])
+# print(nomes[-4])
+# print(nome1)
 
-print (nomes [-1])
-print (nomes [-2])
-print (nomes [-3])
-print (nomes [-4])
-print (nomes)
+# primeira_parte=nomes[0:3]
+# print(primeira_parte)
+# segunda_parte=nomes[3:6]
+# print(segunda_parte)
 
-primeira_parte = nomes [0:3]
-print (primeira_parte)
+# print(len(nomes))
 
-segunda_parte = nomes [3:6]
-print (segunda_parte)
+#TUPLAS:
+nomes_tupla=tuple(nomes)
+print(nomes_tupla)
+#SETS:
+nomes_set=set(nomes)
+print(nomes_set)
+#DICIONÁRIOS:
+nomes_dict={1:'Matheus',
+            2: 'Alice',
+            3: 'Caio',
+            4: 'Larissa',
+            5:'Miguel',
+            6: 'Rafael'}
 
-print (len(nomes))
+
+nomes_att = ["Daniela Sabino", "Rafael Portugal", "Sergio Cabral"]
+sobrenome = []
+# dani = nomes_att [0][8:]
+# rafael = nomes_att [1][7:]
+# sergio = nomes_att [2][6:]
+# sobrenome = [dani,rafael,sergio]
+# print (sobrenome)
+
+for i in nomes_att:
+    sobrenome .append(nomes_att[0])
+print (sobrenome)
+
+nome5='Luciene'
+nome4= 'Miguel'
+nomes_att.insert(2, nome4)
+nomes_att.append(nome5)
+nomes_att.append(nome5)
+
+nomes_att.count(2)
+print(nomes_att)
+
+
+
+
+############ LISTAS
+
+lista01=[100,20,43,23,675,23,12,55]
+#           [0]    [1] [2]  [3] ...
+for i in lista01:
+    print(i,':',type(i))
+
+print(lista01[-1][2])
+
+
+## TUPLAS ###
+
+pares=(40,20,2,18,14,34,96,30,20,58)
+print(pares[3])
+print(pares[3:])
+print(pares[3:8])
+print(len(pares))
+pares=pares+(44,)
+print(pares)
+lista_pares=list(pares)
+print(lista_pares)
+lista_pares.append(102)
+lista_pares.sort()
+lista_pares=tuple(lista_pares)
+print(lista_pares)
+
+### SETS ###
+impares={33,5,17,11,27,11,71,79,99,15}
+print(impares)
+print(type(impares))
+impares_02={11,3,23,83,15,73}
+intersecao=impares.intersection(impares_02)
+print(intersecao)
