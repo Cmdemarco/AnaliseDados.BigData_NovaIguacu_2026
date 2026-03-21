f354584dad4b176b3d5a019efa4d20c24cdb8f34
Cmdemarco <caio.pais@outlook.com>
2026-03-20 21:53:29 -0300

    Material 13/03 & 18/06 - Aula06

--- Aula06/Aula06_130326.py
+++ Aula06/Aula06_130326.py
@@ -122,3 +122,13 @@ print(type(impares))
 impares_02={11,3,23,83,15,73}
 intersecao=impares.intersection(impares_02)
 print(intersecao)
+
+# DICIONÁRIO: Usando o índice como chave (ex: 1: 'Filme A')
+dicionario_filmes = {}
+for i in range(len(dados_entrada)): # Repetição pelo tamanho da lista
+ # Usando o índice (i+1) como CHAVE e o nome como VALOR
+ dicionario_filmes[i + 1] = dados_entrada[i]
+print(f"LISTA (Flexível): {lista_filmes}")
+print(f"TUPLA (Fixa): {tupla_filmes}")
+print(f"SET (Apenas Únicos): {set_filmes}")
+print(f"DICIONÁRIO (Chave: Valor): {dicionario_filmes}")
